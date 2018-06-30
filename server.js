3e82a694c2bc58124b86d98ff946ade2d1aa35c4
Phil S <philipstubbs13@gmail.com>
2018-06-29 22:40:31 -0500

    directory structure setup

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+// const routes = require('./routes');
+// const User = require('./models/User')
+
+// USER AUTH REQUIREMENTS:
+//const passport = require('./passport');
+
+// Server will use port 3001.
+const PORT = process.env.PORT || 3001;
+// Yes, the app uses express.
+const app = express();
+
+// Configure body parser for axios requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// Require all models
+// const db = require('./models');
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+// // Add routes, both API and view
+// app.use(routes);
+
+// Route for retrieving all Users from the db
+// app.get('/user', function (req, res) {
+//     // Find all Users
+//     db.User.find({})
+//         .then(function (dbUser) {
+//             // If all Users are successfully found, send them back to the client
+//             res.json(dbUser);
+//         })
+//         .catch(function (err) {
+//             // If an error occurs, send the error back to the client
+//             res.json(err);
+//         });
+// });
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ballup');
+
+// If deployed, use the deployed database. Otherwise use the local ballup database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ballup";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+    useMongoClient: true
+});
+
+// configurePassport
+// const configurePassport = require('./controllers/passport')
+
+// const passport = configurePassport(app, mongoose, User)
+
+// Add routes, both API and view
+// app.use(routes(passport, User));
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
