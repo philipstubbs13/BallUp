666c9fd8902504fe08d440a1caee9021c550e3ec
Phil S <philipstubbs13@gmail.com>
2018-07-02 23:53:57 -0500

    fixing compile errors

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { Route, Switch } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
- <BrowserRouter>
-  <Switch>
-   <Route path="/" component={App} />
-  </Switch>
- </BrowserRouter>,
- document.getElementById("root")
-);
+ReactDOM.render( < App / > , document.getElementById('root'));
 registerServiceWorker();
