a25ff06fc9a8b3c39882fb163d098508c68bef56
philipstubbs13 <philipstubbs13@gmail.com>
2018-07-05 23:48:02 -0500

    more linting

--- client/src/components/BottomNav/BottomNav.js
+++ client/src/components/BottomNav/BottomNav.js
@@ -10,23 +10,20 @@ import { withStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 // Import material ui icons used on bottom navigation bar.
-import Icon from '@material-ui/core/Icon';
 import Home from '@material-ui/icons/Home';
 import AddCircle from '@material-ui/icons/AddCircle';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import EventAvailable from '@material-ui/icons/EventAvailable';
 import HelpOutline from '@material-ui/icons/HelpOutline';
-// Import material ui grid layout.
-import Grid from '@material-ui/core/Grid';
 
 // Define styling for bottom navigation.
 const styles = {
   root: {
     backgroundColor: '#000022',
-    borderTop: '3px solid #FA8320',  
+    borderTop: '3px solid #FA8320',
   },
   navIcon: {
-      color: 'white',
+    color: 'white',
   },
 };
 
@@ -45,11 +42,11 @@ class BottomNav extends React.Component {
 
     return (
       <BottomNavigation value={value} onChange={this.handleChange} className={classes.root} justify="center">
-        <BottomNavigationAction label="Home" value="home" icon={<Home />} className={classes.navIcon} component={Link} to="/home"/>
-        <BottomNavigationAction label="Create game" value="create_game" icon={<AddCircle />} className={classes.navIcon} component={Link} to="/newgame"/>
-        <BottomNavigationAction label="Upcoming games" value="games" icon={<EventAvailable />} className={classes.navIcon} component={Link} to="/upcominggames"/>
-        <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} className={classes.navIcon} component={Link} to="/nearby"/>
-        <BottomNavigationAction label="Help" value="help" icon={<HelpOutline />} className={classes.navIcon} component={Link} to="/help"/>
+        <BottomNavigationAction label="Home" value="home" icon={<Home />} className={classes.navIcon} component={Link} to="/home" />
+        <BottomNavigationAction label="Create game" value="create_game" icon={<AddCircle />} className={classes.navIcon} component={Link} to="/newgame" />
+        <BottomNavigationAction label="Upcoming games" value="games" icon={<EventAvailable />} className={classes.navIcon} component={Link} to="/upcominggames" />
+        <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} className={classes.navIcon} component={Link} to="/nearby" />
+        <BottomNavigationAction label="Help" value="help" icon={<HelpOutline />} className={classes.navIcon} component={Link} to="/help" />
       </BottomNavigation>
     );
   }
