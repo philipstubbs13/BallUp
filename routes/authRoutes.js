c0b8135bdccce4bb7ac5b34ffad8ed4df1a48510
philipstubbs13 <philipstubbs13@gmail.com>
2018-07-08 18:36:41 -0500

    user successfully saved to database

---
+++ routes/authRoutes.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+  // Route handlers
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email'],
+    }),
+  );
+
+  app.get(
+    '/auth/google/callback', passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/home');
+    },
+  );
+
+  // Log user out of application
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // Current user request handler
+  app.get('/api/current_user', (req, res) => {
+    // res.send(req.session);
+    res.send(req.user);
+  });
+};
